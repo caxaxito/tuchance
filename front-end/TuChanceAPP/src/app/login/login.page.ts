0dbdcabd6801935c82721a69ecac4d60065d9d23
Carlos Caxaj <caxaj17@gmail.com>
2025-01-30 10:04:25 -0600

    Implementacion de seguridad con JWT y manejo de redireccion a los tabs

--- front-end/TuChanceAPP/src/app/login/login.page.ts
+++ front-end/TuChanceAPP/src/app/login/login.page.ts
@@ -1,44 +1,19 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  standalone: false,
+  standalone: false
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  username = '';
+  password = '';
 
-  ngOnInit(): void {
-    
+  constructor(private authService: AuthService) {}
+
+  onLogin() {
+    this.authService.login(this.username, this.password);
   }
-
-  loginData = {
-    username: '',
-    password: ''
-  };
-
-  constructor(private http: HttpClient, private router: Router) {}
-
-  onSubmit() {
-    console.log('Datos de inicio de sesión:', this.loginData);
-    this.login();
-
-  }
-
-  login() {
-    
-
-    this.http.post<any>('http://127.0.0.1:8000/api/obtener/token/', this.loginData)
-      .subscribe(response => {
-        localStorage.setItem('auth_token', response.token);  // Guardar token en localStorage
-        console.log('Token recibido:', response.token);
-        this.router.navigate(['/tabs']);  // Redirigir a la página de Tabs
-      }, error => {
-        console.error('Error al iniciar sesión:', error);
-        
-      });
-  }
-
 }
